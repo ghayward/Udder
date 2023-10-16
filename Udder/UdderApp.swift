8184ceba9d31a4dadc929b31441ec944d96c7697
George Hayward <gjhayward@gmail.com>
2023-10-15 22:51:27 -0400

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by George Hayward on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
