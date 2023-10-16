d16302938a0c66ee221d998fee30726c161fe5d6
George Hayward <gjhayward@gmail.com>
2023-10-15 23:21:10 -0400

    add milk tagline

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Udder")
+            Text("Milk Design is My Galaxy")
         }
         .padding()
     }
